70fe29e12d112fceec8095e2e3fb30cfbb0c9a9a
Steve Doty <stevedoty16@gmail.com>
2020-08-12 10:57:27 -0700

    build with docs folder for github.io

---
+++ docs/precache-manifest.652ff519173bef1f3d8e2f5e51ab6bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10b497157b16532a244f5d9e9d98ecf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4677f732917768e1d45b",
+    "url": "/static/css/main.415ab16a.chunk.css"
+  },
+  {
+    "revision": "260a5cf640bcccd0a663",
+    "url": "/static/js/2.32318531.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32318531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4677f732917768e1d45b",
+    "url": "/static/js/main.de5d0ee5.chunk.js"
+  },
+  {
+    "revision": "ac669d4e34022ba6ce71",
+    "url": "/static/js/runtime-main.c7082d76.js"
+  }
+]);
